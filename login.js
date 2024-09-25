2b0962dc8ffb1c251ae1fca7f27344ec6871042a
Seu Nome <seu-email@exemplo.com>
2024-09-25 09:05:12 -0400

    mudança

--- login.js
+++ login.js
@@ -1,12 +1,29 @@
-document.getElementById('loginForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const username = document.getElementById('login__username').value;
-    const password = document.getElementById('login__password').value;
-    const validUsername = 'thayane';
-    const validPassword = '123';
-if (username === validUsername && password === validPassword) {
+document.getElementById('loginButton').addEventListener('click', function(event) {
+    
+    document.getElementById('emailError').textContent = '';
+    document.getElementById('passwordError').textContent = '';
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    let valid = true;
+    const myEmail = 'thayane520.biel@gmail.com'; 
+    const myPassword = '123'; 
+    if (!validateEmail(email)) {
+        document.getElementById('emailError').textContent = 'Por favor, insira um e-mail válido.';
+        valid = false;
+    } else if (email !== myEmail) {
+        document.getElementById('emailError').textContent = 'E-mail incorreto.';
+        valid = false;
+    }
+    if (password !== myPassword) {
+        document.getElementById('passwordError').textContent = 'Senha incorreta.';
+        valid = false;
+    }
+    if (valid) {
         window.location.href = './form.html';
-    } else {
-        alert('Usuário ou senha inválidos. Tente novamente.');
     }
 });
+
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+}

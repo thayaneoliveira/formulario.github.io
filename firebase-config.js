e4bd29486b456619da8d7edfef9f80fa7e37bdde
Seu Nome <seu-email@exemplo.com>
2024-10-24 16:19:09 -0400

    atual

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,6 @@
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
-
 
 const firebaseConfig = {
     apiKey: "AIzaSyBm4CTZtfzMkuUojKo1ZA6VRATpfeBQ1Oo",
@@ -10,10 +9,9 @@ const firebaseConfig = {
     storageBucket: "jornadaqa-full.appspot.com",
     messagingSenderId: "671761111796",
     appId: "1:671761111796:web:a4aedcdd224f5b1d225ca1"
-  };
+};
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 const auth = getAuth(app);
 
-export { db, auth };
+export { auth };
--- login.js
+++ login.js
@@ -6,6 +6,7 @@ import {
 
 const provider = new GoogleAuthProvider();
 
+// Formulário de login
 document
   .getElementById("loginForm")
   .addEventListener("submit", function (event) {
@@ -18,22 +19,27 @@ document
     const validPassword = "123456";
 
     if (email === validEmail && password === validPassword) {
-
-      sessionStorage.setItem('isAuthenticated', 'true');
-
+      sessionStorage.setItem("isAuthenticated", "true");
       window.location.href = "./menu.html";
     } else {
       alert("Email ou senha incorretos. Por favor, tente novamente.");
     }
   });
 
+// Login com Google
 document.getElementById("googleLogin").addEventListener("click", function () {
   signInWithPopup(auth, provider)
     .then((result) => {
-      console.log(result.user);
+      // O usuário logado
+      const user = result.user;
+      console.log("Usuário logado com Google:", user);
+      sessionStorage.setItem("isAuthenticated", "true");
       window.location.href = "./menu.html";
     })
     .catch((error) => {
       console.error("Erro ao fazer login com Google:", error);
+      alert(
+        "Erro ao tentar fazer login com o Google. Verifique o console para mais detalhes."
+      );
     });
 });

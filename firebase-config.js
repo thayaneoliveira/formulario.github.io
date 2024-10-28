9c02e49cf413035847870563b2f0f33c8b5563dc
Seu Nome <seu-email@exemplo.com>
2024-10-28 10:16:55 -0400

    commit init

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,19 @@
-// firebase-config.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js"; 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBm4CTZtfzMkuUojKo1ZA6VRATpfeBQ1Oo",
-    authDomain: "jornadaqa-full.firebaseapp.com",
-    projectId: "jornadaqa-full",
-    storageBucket: "jornadaqa-full.appspot.com",
-    messagingSenderId: "671761111796",
-    appId: "1:671761111796:web:a4aedcdd224f5b1d225ca1"
+  apiKey: "AIzaSyBm4CTZtfzMkuUojKo1ZA6VRATpfeBQ1Oo",
+  authDomain: "jornadaqa-full.firebaseapp.com",
+  projectId: "jornadaqa-full",
+  storageBucket: "jornadaqa-full.appspot.com",
+  messagingSenderId: "671761111796",
+  appId: "1:671761111796:web:a4aedcdd224f5b1d225ca1",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app); 
 
-export { auth };
+export { auth, db }; 
--- form.js
+++ form.js
@@ -1,52 +1,60 @@
-import { db } from './firebase-config.js'; 
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
+
+import { db } from "./firebase-config.js"; 
+import {
+  collection,
+  addDoc,
+} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
 
-(function(){
-    emailjs.init("SoYFGdC765nftygyX"); 
-})();
+emailjs.init("SoYFGdC765nftygyX");
 
 
-document.getElementById("loginForm").addEventListener("submit", function(event) {
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault(); 
 
-    const userName = document.getElementById("txt").value; 
-    const userEmail = document.getElementById("email").value; 
-    const userPhone = document.getElementById("broj").value; 
-    const userAge = document.getElementById("password").value; 
+    
+    const userName = document.getElementById("txt").value;
+    const userEmail = document.getElementById("email").value;
+    const userPhone = document.getElementById("broj").value;
+    const userAge = document.getElementById("password").value;
 
+    
     const templateParams = {
-        from_name: userName, 
-        to_email: userEmail, 
-        phone: userPhone,    
-        age: userAge,        
+      from_name: userName,
+      to_email: userEmail,
+      phone: userPhone,
+      age: userAge,
     };
 
-    emailjs.send("gmailMessage", "template_febra45", templateParams)
-        .then(function(response) {
-            console.log("Email enviado com sucesso!", response.status, response.text);
-            alert("Formulário enviado com sucesso!");
+  
+    emailjs
+      .send("gmailMessage", "template_febra45", templateParams)
+      .then(function (response) {
+        console.log(
+          "Email enviado com sucesso!",
+          response.status,
+          response.text
+        );
+        alert("Formulário enviado com sucesso!");
 
-            addDoc(collection(db, "userSubmissions"), {
-                name: userName,
-                email: userEmail,
-                phone: userPhone,
-                age: userAge,
-                timestamp: new Date() 
-            })
-            .then(function() {
-                // console.log("Dados salvos no Firestore com sucesso!");
-                // alert("Dados salvos no banco de dados com sucesso!");
-
-                
-                document.getElementById("loginForm").reset();
-            })
-            .catch(function(error) {
-                console.error("Erro ao salvar os dados no Firestore:", error);
-                alert("Erro ao salvar os dados no banco de dados.");
-            });
-        }, function(error) {
-            console.log("Erro ao enviar o e-mail:", error);
-            alert("Erro ao enviar formulário. Tente novamente.");
+       
+        return addDoc(collection(db, "userSubmissions"), {
+          name: userName,
+          email: userEmail,
+          phone: userPhone,
+          age: userAge,
+          timestamp: new Date(),
         });
-});
+      })
+      .then(function () {
+        
+        document.getElementById("loginForm").reset();
+       
+      })
+      .catch(function (error) {
+        console.error("Erro ao enviar o e-mail ou salvar no Firestore:", error);
+        alert("Erro ao enviar formulário. Tente novamente.");
+      });
+  });

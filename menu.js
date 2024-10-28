e9b97be1b2e2910ed52f4131555c9be35648d1a4
Seu Nome <seu-email@exemplo.com>
2024-10-28 09:46:54 -0400

    atualizaçao

--- menu.js
+++ menu.js
@@ -1,11 +1,11 @@
-// Importando a autenticação do Firebase
+
 import { auth } from "./firebase-config.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 
-// Verifica o estado da autenticação
+
 onAuthStateChanged(auth, (user) => {
   if (!user) {
-    // Se o usuário não estiver logado, redireciona para a página de login
+    
     window.location.href = "login.html";
   }
 });
